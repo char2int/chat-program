fd3e6d0cd890dbe42a0590aa8da1c1552f679864
SamzyDev <138089799+char2int@users.noreply.github.com>
2023-07-23 20:17:44 -0500

    d

---
+++ HashChecker/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+
+namespace HashChecker
+{
+    internal class Program
+    {
+        private static WebClient client2 = new WebClient();
+        private static string latest_hash = client2.DownloadString("https://raw.githubusercontent.com/char2int/chat-program/main/latest.HASH").Replace("\n", "");
+
+        static void Main(string[] args)
+        {
+            Console.Title = "HashChecker | ConsoleMessenger";
+            Console.WriteLine("Input Program: ");
+            string program = Console.ReadLine().Replace("\"", "");
+            Console.Clear();
+            if (latest_hash == CalculateMD5(program))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Hashes match!");
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+            Console.WriteLine("Latest Hash:");
+            Console.WriteLine(latest_hash);
+            Console.WriteLine("Current Hash: ");
+            Console.WriteLine(CalculateMD5(program));
+            Console.ReadKey();
+        }
+
+        static string CalculateMD5(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                }
+            }
+        }
+    }
+}
--- client/Program.cs
+++ client/Program.cs
@@ -1,11 +1,13 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -13,13 +15,22 @@ namespace client
 {
     internal class Program
     {
+        private const bool dev = true;
         private static WebClient client2 = new WebClient();
         private static string url = client2.DownloadString("https://raw.githubusercontent.com/char2int/chat-program/main/address.txt").Replace("\n", "");
+        private static string latest_hash = client2.DownloadString("https://raw.githubusercontent.com/char2int/chat-program/main/latest.HASH").Replace("\n", "");
         private static readonly HttpClient client = new HttpClient();
         private static string token { get; set; }
         private static string old_response { get; set; }
         static async Task Main(string[] args)
         {
+            Console.Title = "Checking for Update | ConsoleMessenger";
+            if (CalculateMD5(Process.GetCurrentProcess().MainModule.FileName) != latest_hash && !dev)
+            {
+                Console.WriteLine("Version Outdated! Opening GitHub...");
+                Process.Start("https://github.com/char2int/chat-program/releases/latest");
+                Process.GetCurrentProcess().Kill();
+            }
             Console.Title = "Log In | ConsoleMessenger";
             Console.WriteLine("Username: ");
             token = Console.ReadLine();
@@ -31,6 +42,18 @@ namespace client
             {
                 string message = Console.ReadLine();
                 await SendMessage(message);
+            }
+        }
+
+        static string CalculateMD5(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                }
             }
         }
 
